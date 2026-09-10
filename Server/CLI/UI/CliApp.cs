a5ada23f9ed82f63b6565c36bff7095aac9c095a
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2026-09-10 11:59:14 +0200

    cli app loop

--- Server/CLI/UI/CliApp.cs
+++ Server/CLI/UI/CliApp.cs
@@ -17,6 +17,32 @@ public class CliApp
 
     public async Task StartAsync()
     {
-        throw new NotImplementedException();
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("\n--- Main menu ---");
+            Console.WriteLine("\n1) Manage users");
+            Console.WriteLine("\n2) Manage posts");
+            Console.WriteLine("\n0) Exit");
+            Console.WriteLine("> ");
+
+            string? choice = Console.ReadLine();
+            if (choice is null) break;
+
+            switch (choice.Trim().ToLower())
+            {
+                case "1":
+                    break;
+                case "2": ;
+                    break;
+                case "0":
+                    running = false;
+                    break;
+                default: 
+                    Console.WriteLine($"Invalid choice: {choice}");
+                    break;
+            }
+            
+        }
     }
 }
