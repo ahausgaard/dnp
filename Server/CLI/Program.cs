57a3d23b540996829b33629927b64a89861064a7
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2026-09-10 10:51:52 +0200

    CLI init

---
+++ Server/CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+Console.WriteLine("Hello, World!");
+IUserRepository userRepository = new UserInMemoryRepository();
+ICommentRepository commentRepository = new CommentInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+
+ClipApp cliApp = new ClipApp(userRepository, commentRepository, postRepository);
+await cliApp.StartAsync();
